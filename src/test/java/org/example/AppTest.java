1621badd35946ff570320be8caba35ef475b3bda
Aref Hosseini <aref.hosseini.9571@gmail.com>
2026-02-27 21:13:49 +0200

    Initu=ialize Maven project with JUnit 5

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void addition_works() {
+        int result = 1 + 1;
+        assertEquals(2, result);
+    }
+}
